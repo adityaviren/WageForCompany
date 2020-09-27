6f2a8d86200c66d6c85536dbeb0188ee496f0167
adityaviren <adityaviren@gmail.com>
2020-09-27 19:09:10 +0530

    Add welcome message

--- src/wageforcompany/WageForCompany.java
+++ src/wageforcompany/WageForCompany.java
@@ -4,7 +4,7 @@ public class WageForCompany {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to company wage programme");
 	}
 
 }
